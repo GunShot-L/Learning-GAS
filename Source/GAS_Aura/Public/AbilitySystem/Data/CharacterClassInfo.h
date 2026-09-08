95b64830e10c8573fa95bd8f47246558c5532e4d
zsyuue <2280856220@qq.com>
2026-09-08 23:46:43 +0800

    新增怪物属性GE

---
+++ Source/GAS_Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+﻿// Copyright Linwenjie
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM()
+enum class ECharacterClass : uint8
+{
+	Elementalist, // 奥术师、元素师
+	Warrior, // 战士
+	Ranger // 游侠
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ *
+ */
+UCLASS()
+class GAS_AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+	
+	FCharacterClassDefaultInfo GetClassDefaultInfo(const ECharacterClass CharacterClass);
+};
