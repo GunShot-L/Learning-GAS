30f7b1297f6b75eff5792dee102c6ad6775b1cfe
zsyuue <2280856220@qq.com>
2026-09-09 16:01:01 +0800

    为伤害设置曲线表格并读取到GE里

--- Source/GAS_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/GAS_Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -46,7 +46,8 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		const FGameplayEffectSpecHandle SpecHandle = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), SourceASC->MakeEffectContext());
 		
 		FAuraGameplayTags GameplayTags = FAuraGameplayTags::Get();
-		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, 50.f);
+		const float ScaleDamage = Damage.GetValueAtLevel(GetAbilityLevel());
+		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandle, GameplayTags.Damage, ScaleDamage);
 		
 		Projectile->DamageEffectSpecHandle = SpecHandle;
 		
