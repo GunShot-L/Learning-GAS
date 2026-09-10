8f33a8aa5557dff59535fb8a506ea5064c3494f6
zsyuue <2280856220@qq.com>
2026-09-10 20:11:42 +0800

    新增伤害计算类，专门用于结算实际伤害量

---
+++ Source/GAS_Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,74 @@
+﻿// Copyright Linwenjie
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	// 用宏来自动生成属性快照定义
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
+	
+	AuraDamageStatics()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+	}
+	
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	// 静态方法，返回属性快照
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	// 这里捕获属性快照
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+	RelevantAttributesToCapture.Add(DamageStatics().BlockChanceDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	// 利用捕获到的属性快照信息，计算实际伤害
+	
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+	
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+	
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+	
+	// 根据施法者获取实际伤害
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+	
+w	float TargetBlockChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef, EvaluateParameters, TargetBlockChance);
+	TargetBlockChance = FMath::Max<float>(TargetBlockChance, 0.f);
+	const bool bBlocked  = FMath::RandRange(1, 100) < TargetBlockChance;
+	Damage = bBlocked ? Damage / 2.f : Damage; // 伤害格挡成功后免减一半
+	
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor, 0.0f);
+	float DefendRating = TargetArmor / (TargetArmor + 100.f);
+	Damage = Damage * FMath::Clamp((1.f - DefendRating), 0.f, 1.f);
+	
+	const FGameplayModifierEvaluatedData EvaluationData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
+	OutExecutionOutput.AddOutputModifier(EvaluationData);
+}
